2123ef821bcc600b7a616cdddec6fcd86f5ad1ee
Eric <eric.vargas.rss@gmail.com>
2021-11-26 09:53:25 -0600

    fix logo sidebar adminlte

--- config/adminlte.php
+++ config/adminlte.php
@@ -45,8 +45,8 @@ return [
     |
     */
 
-    'logo' => '',
-    'logo_img' => 'img/favicon.png',
+    'logo' => 'P',
+    'logo_img' => '',
     'logo_img_class' => 'brand-image elevation-3 align-items-center d-flex justify-content-center',
     'logo_img_xl' => 'img/logo_sidebar.png',
     'logo_img_xl_class' => 'brand-image-xs',
